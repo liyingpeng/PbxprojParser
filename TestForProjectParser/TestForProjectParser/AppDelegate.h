eed5dfa7b817d2288c2a4d7f6390709bacbf0bc4
liyingpeng <liyingpeng@p1.com>
2019-07-21 23:28:56 +0800

    add test project

---
+++ TestForProjectParser/TestForProjectParser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestForProjectParser
+//
+//  Created by liyingpeng on 2019/7/21.
+//  Copyright © 2019 liyingpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestForProjectParser/TestForProjectParser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestForProjectParser
+//
+//  Created by liyingpeng on 2019/7/21.
+//  Copyright © 2019 liyingpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
