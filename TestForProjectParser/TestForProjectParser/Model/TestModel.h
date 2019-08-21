9daefc0861c562f36054385b67efdebc34b0f55c
liyingpeng <liyingpeng@p1.com>
2019-08-21 16:45:24 +0800

    add test model

---
+++ TestForProjectParser/TestForProjectParser/Model/TestModel.h
@@ -0,0 +1,17 @@
+//
+//  TestModel.h
+//  TestForProjectParser
+//
+//  Created by 李应鹏 on 2019/8/21.
+//  Copyright © 2019 liyingpeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestForProjectParser/TestForProjectParser/View/TestView.m
@@ -0,0 +1,21 @@
+//
+//  TestView.m
+//  TestForProjectParser
+//
+//  Created by 李应鹏 on 2019/8/21.
+//  Copyright © 2019 liyingpeng. All rights reserved.
+//
+
+#import "TestView.h"
+
+@implementation TestView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
